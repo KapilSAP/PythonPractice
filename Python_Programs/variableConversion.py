aae90bd5b56c01cbde91d1932c377f644651fa9d
Kapil2703 <kapilgupta273@gmail.com>
2024-05-07 08:52:17 +0530

    comparison_operators_day02

--- Python_Programs/variableConversion.py
+++ Python_Programs/variableConversion.py
@@ -1,13 +1,13 @@
-#explicit type conversion
+# explicit type conversion
 
-string_num = '5.3'
+string_num = "5.3"
 print(string_num)
 
-float_num=float(string_num) #Convert any number to FLoat
+float_num = float(string_num)  # Convert any number to FLoat
 print(float_num)
 
-int_num=int(float_num) #Convert any number to Int
+int_num = int(float_num)  # Convert any number to Int
 print(int_num)
 
-string_num=str(int_num)
+string_num = str(int_num)  # Convert any number to String
 print(string_num)
