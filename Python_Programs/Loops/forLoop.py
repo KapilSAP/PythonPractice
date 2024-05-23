24ab28bfa46f56eaf63bdb3e2d7347048a9807ca
Kapil2703 <kapilgupta273@gmail.com>
2024-05-23 19:35:31 +0530

    day07_forLoop

---
+++ Python_Programs/Loops/forLoop.py
@@ -0,0 +1,12 @@
+lst = [2, 3, 4, 5, 7]
+
+for num in lst:
+    print(num)
+
+print("Cheers")
+
+
+# Access the indices while iterating over a list
+
+for index, num in enumerate(lst):
+    print(index)
--- Python_Programs/Loops/iterables.py
+++ Python_Programs/Loops/iterables.py
@@ -4,8 +4,25 @@
 lst = [23, 45, 67]
 i = 0
 
+
+# While Loop Syntax
 while i < len(lst):  # when i = len on list it exit the loop
     print(f"{i} element is {lst[i]}")  # Print element on the each index of the list
     i = i + 1  # increment index
 
 print("Moving On!")
+
+# For loop Syntax
+for num in lst:
+    print(num)
+
+print("Cheers")
+
+
+# Access the indices while iterating over a list in for loop
+
+for index, num in enumerate(lst):
+    print(index)  # this will print index
+    print(f"{index}.{num}")  # this will print value on the index
+
+# Dictionary Syntax
