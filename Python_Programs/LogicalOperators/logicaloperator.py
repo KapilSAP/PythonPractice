cce5aac6a561b6d69513a4981be5e1cb21eb4ce2
KapilSAP <kapilsap273@gmail.com>
2024-12-30 18:05:11 +0530

    addedcomment

--- Python_Programs/LogicalOperators/logicaloperator.py
+++ Python_Programs/LogicalOperators/logicaloperator.py
@@ -1,17 +1,20 @@
 # there are three logical operators
-# and, or, not,
+# and, or, not, 
+# and operator check if both the expressions are true
+# or operator check if either expression is true
+# not operator check if the expression is false
 
-num = int(input("Please enter a number in between 1-100: "))
+num = int(input("Please enter a number in between 1-100: ")) # input is used to take input from user in integer
 
 if num > 20 and num < 50:  # and operator check if both the expressions are true
     print("AND condition is true")
 elif num > 20 or num < 50:  # or operator check if either expression is true
     print("Or Condition is true")
 
-if num == 20 or num == 50:
+if num == 20 or num == 50: # check if num is equals to 20 or 50
     print("Equals Condition is true")
-elif num != 20 or num != 50:
+elif num != 20 or num != 50: #  check if num is not equals to 20 or 50
     print("Not Equals Condition is true")
 
-if not num != 30:
+if not num != 30:  # not operator check if the expression is false    
     print("Num is equals to 30")
--- Python_Programs/LogicalOperators/logicaloperatorEx1.py
+++ Python_Programs/LogicalOperators/logicaloperatorEx1.py
@@ -1,20 +1,26 @@
-balance = float(input("Please enter your account balance: "))
-age = int(input("Please enter your age: "))
-have_job_input = input("Please enter 'Y' if you have job: ")
+# logical operator example
+# This program will ask user to enter job, account balance, age  
+# which will be checked against some conditions to determine
+# if he/she is eligible for loan or not.
+#
 
-if have_job_input == "Y" or have_job_input == "yes":
-    have_job = True
+balance = float(input("Please enter your account balance: ")) # float is used to convert input to float
+age = int(input("Please enter your age: ")) # int is used to convert input to integer
+have_job_input = input("Please enter 'Y' if you have job: ") # input is used to take input from user
+
+if have_job_input == "Y" or have_job_input == "yes": # check if user has job
+    have_job = True     # if user has job then set have_job to True
 else:
-    have_job = False
+    have_job = False     #  if user does not have job then set have_job to False
 
-if age > 18 and age < 80:
-    print("You can have special discount")
+if age > 18 and age < 80: # check if age is in between 18 and 80
+    print("You can have special discount") # if age is in between 18 and 80 then print this message
 
-if balance < 20:
-    print("You are in debt")
+if balance < 20: # check if balance is less than 20
+    print("You are in debt") # if balance is less than 20 then print this message
 else:
-    print("you are not in debt")
-    if balance < 2000 or age > 60 or age < 18 or not have_job:
-        print("You can not have loan")
+    print("you are not in debt") #  if balance is not less than 20 then print this message
+    if balance < 2000 or age > 60 or age < 18 or not have_job: #    check if balance is less than 2000 or age is greater than 60 or age is less than 18 or user does not have job
+        print("You can not have loan") #    if any of the above condition is true then print this message
     else:
-        print("You can ask for loan")
+        print("You can ask for loan") #   if none of the above condition is true then print this message
